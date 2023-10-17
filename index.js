57aeb42452236d3878f424518ce7eef6c7d1e615
Danson Lino <danson.lino@student.moringaschool.com>
2023-10-17 18:42:19 +0300

    let's commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// function declaration called divide
+const div = function(){
+    return 2000/100;
+};
+
+// arrow function called square
+let x = 2;
+const square = (x) => x ** 2;
+console.log(square(x));
+
+// arrow function called add
+let a = 3;
+let b = 4;
+const add = (a,b) => {
+    return a + b;
+}
+console.log(add(a,b))
+
+
+
